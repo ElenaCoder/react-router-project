b5b475ba0fc8ded2c2ba889c3ac7cfa43af9b30f
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-11-07 15:46:31 +0200

    feat: add loading careers handling function to Careers component

--- src/pages/careers/Careers.jsx
+++ src/pages/careers/Careers.jsx
@@ -1,11 +1,22 @@
-function Careers() {
+import { Link, useLoaderData } from 'react-router-dom';
+
+export default function Careers() {
+    const careers = useLoaderData();
     return (
-      <div className="careers">
+        <div className='careers'>
+            {careers.map((career) => (
+                <Link to='/' key={career.id}>
+                    <p>{career.title}</p>
+                    <p>Based in {career.location}</p>
+                </Link>
+            ))}
+        </div>
+    );
+}
 
-      </div>
-    )
-  }
+// loader function
+export const careersLoader = async () => {
+    const res = await fetch('http://localhost:4000/careers');
 
-
-
-  export default  Careers;
+    return res.json();
+};
