dfce566d23e96888aa7c30d3b29732cf8daeaa72
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-11-07 14:56:02 +0200

    feat: update router with Career and CareersLayout components

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,12 @@ import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Faq from './pages/help/Faq';
 import Contact from './pages/help/Contact';
+import Careers from './pages/careers/Careers';
 
 //layouts
 import RootLayout from './layouts/RootLayout';
 import HelpLayout from './layouts/HelpLayout';
+import CareersLayout from './layouts/CareersLayout';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -24,6 +26,10 @@ const router = createBrowserRouter(
             <Route path='help' element={<HelpLayout />}>
                 <Route path='faq' element={<Faq />} />
                 <Route path='contact' element={<Contact />} />
+            </Route>
+
+            <Route path='careers' element={<CareersLayout />}>
+                <Route index element={<Careers />} />
             </Route>
 
             <Route path='*' element={<NotFound />} />
