f308e251eab37315f2e2c227687f6a8941eb8520
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-11-08 14:47:02 +0200

    feat: fill CareerDetails template with loader data

--- src/pages/careers/CareerDetails.jsx
+++ src/pages/careers/CareerDetails.jsx
@@ -1,11 +1,32 @@
-import { useParams } from 'react-router-dom'
+// import { useParams } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 
 export default function CareerDetails() {
-  const {id} = useParams();
+    // const { id } = useParams();
+
+    const career = useLoaderData();
 
     return (
-      <div className="career-details">
-        <h2>{ id }</h2>
-      </div>
-    )
-  }
+        <div className='career-details'>
+            <h2>Career Details for {career.title}</h2>
+            <p>Starting salary: {career.salary}</p>
+            <p>Location: {career.location}</p>
+            <div className='details'>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Rerum dolor assumenda, quas odio optio, distinctio ab sunt
+                    unde incidunt ipsum omnis amet magnam accusantium aut!
+                    Excepturi, cupiditate iusto!
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export const careerDetailsLoader = async ({ params }) => {
+    const { id } = params;
+
+    const res = await fetch('http://localhost:4000/careers/' + id);
+
+    return res.json();
+};
