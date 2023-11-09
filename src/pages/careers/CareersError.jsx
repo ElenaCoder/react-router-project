826ecc2d32064969272ccd259ecc2b7c113cf4ee
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-11-09 23:31:34 +0200

    feat: create CareersError component for errorElement props

---
+++ src/pages/careers/CareersError.jsx
@@ -0,0 +1,15 @@
+import { Link, useRouteError } from 'react-router-dom';
+
+function CareersError() {
+    const error = useRouteError();
+
+    return (
+        <div className='careers-error'>
+            <h2>Error</h2>
+            <p>{error.message}</p>
+            <Link to='./careers'>Back to the Careers</Link>
+        </div>
+    );
+}
+
+export default CareersError;
