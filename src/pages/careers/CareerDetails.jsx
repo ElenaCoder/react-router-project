3f2851d12586efab1a524b00decc4eb44599e17e
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-11-09 23:48:13 +0200

    feat: add error handling in careerDetailsLoader function

--- src/pages/careers/CareerDetails.jsx
+++ src/pages/careers/CareerDetails.jsx
@@ -28,5 +28,9 @@ export const careerDetailsLoader = async ({ params }) => {
 
     const res = await fetch('http://localhost:4000/careers/' + id);
 
+    if (!res.ok) {
+        throw Error('Career not found.');
+    }
+
     return res.json();
 };
